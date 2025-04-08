a4eca45957f680b88b270608a1f4426a1c40fc31
Pete Misik <mirtec001@gmail.com>
2025-04-08 14:01:53 -0400

    Add login functionality Change new view

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.Input;
 using DubClubTracker.Models;
@@ -10,22 +11,50 @@ namespace DubClubTracker.ViewModels;
 public partial class MainWindowViewModel : ViewModelBase
 {
     public List<TimeModel> YourTime { get; set; } = [];
-    private string _success = string.Empty;
-    public string Success
+
+    private string _username = string.Empty;
+    private string _password = string.Empty;
+
+    private string _successMessage = string.Empty;
+    public string SuccessMessage
     {
-        get => _success;
+        get
+        {
+            return _successMessage;
+        }
+
         set
         {
-            _success = value;
-            OnPropertyChanged(nameof(Success));
+            _successMessage = value;
+            OnPropertyChanged(nameof(SuccessMessage));
         }
     }
 
+    public string Username
+    {
+        get => _username;
+        set
+        {
+            _username = value;
+            OnPropertyChanged(nameof(Username));
+        }
+    }
+
+    public string Password
+    {
+        get => _password;
+        set 
+        {
+            _password = value;
+            OnPropertyChanged(nameof(Password));
+        }
+    }
+    
     private object _currentView = new SplashView(); // Default view is the splash screen
     public object CurrentView
     {
         get => _currentView;
-        set => SetProperty(ref _currentView, value);
+        set => SetProperty(ref _currentView, value, nameof(CurrentView));
     }
 
     private bool _hasLoggedIn = false;
@@ -34,6 +63,7 @@ public partial class MainWindowViewModel : ViewModelBase
         get => _hasLoggedIn;
         set
         {
+            Console.WriteLine($"HasLoggedIn: {value}");
             _hasLoggedIn = value;
             OnPropertyChanged(nameof(HasLoggedIn));
         }
@@ -41,6 +71,7 @@ public partial class MainWindowViewModel : ViewModelBase
 
     public MainWindowViewModel()
     {
+        // Switch to the main view after login
         ShowSplashScreen();
     }
 
@@ -49,12 +80,17 @@ public partial class MainWindowViewModel : ViewModelBase
         CurrentView = new SplashView();
         await Task.Delay(2000); // Simulate a delay for the splash screen
         CurrentView = new LoginView(); // Switch to the main view after the delay
-
-        if (_hasLoggedIn)
-        {
-            CurrentView = new MainContentView(); // Switch to the main view after login
-        }
     }
 
-    
+    [RelayCommand]    
+    private async Task Login()
+    {
+        Console.WriteLine($"Username: {Username}, Password: {Password}");
+        Console.WriteLine("Login button clicked!"); 
+        SuccessMessage = "Login successful!";
+        await Task.Delay(1000);
+
+        CurrentView = new MainContentView(); // Switch to the main view after login
+        
+    }
 }
--- ViewModels/ViewModelBase.cs
+++ ViewModels/ViewModelBase.cs
@@ -1,7 +1,25 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace DubClubTracker.ViewModels;
 
-public class ViewModelBase : ObservableObject
+public class ViewModelBase : INotifyPropertyChanged
 {
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    
+    protected void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+
+    protected void SetProperty<T>(ref T field, T value, string propertyName)
+    {
+        if (!EqualityComparer<T>.Default.Equals(field, value))
+        {
+            field = value;
+            OnPropertyChanged(propertyName);
+        }
+    }
 }
